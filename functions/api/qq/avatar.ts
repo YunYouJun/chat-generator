ca4a46397c6631add47b8bd9caf03607b567aa43
YunYouJun <me@yunyoujun.cn>
2025-02-16 20:55:57 +0800

    refactor: use cache for rumetime when edgeone

--- functions/api/qq/avatar.ts
+++ functions/api/qq/avatar.ts
@@ -1,9 +1,3 @@
-import { Buffer } from 'node:buffer'
-import { LRUCache } from 'lru-cache'
-
-// LRU cache
-const cache = new LRUCache({ max: 1000 })
-
 function wrapBase64(str: string) {
   return `data:image/png;base64,${str}`
 }
@@ -23,16 +17,21 @@ export async function onRequest({ request }: { request: Request }) {
     }
   }
 
-  if (cache.has(url as string)) {
-    return wrapBase64(cache.get(url) as string)
+  const cache = await caches.open('default')
+
+  const cacheBase64 = await cache.match(url as string).then(res => res?.text())
+  if (cacheBase64) {
+    return wrapBase64(cacheBase64)
   }
   else {
     const imgUrl = new URL(url as string)
     const res = await fetch(imgUrl)
     const buffer = await res.arrayBuffer()
-    const base64 = Buffer.from(buffer).toString('base64')
+    // const base64 = Buffer.from(buffer).toString('base64')
+    const base64 = btoa(String.fromCharCode(...new Uint8Array(buffer)))
 
-    cache.set(url, base64)
+    // cache.set(url, base64)
+    cache.put(url, new Response(base64))
     return wrapBase64(base64)
   }
 }
